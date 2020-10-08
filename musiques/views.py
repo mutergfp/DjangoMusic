dcd665fdade4d0661bdb8540cc9c46589c303b06
Erwan Gay <gay@info07-21.iut45.univ-orleans.fr>
2020-10-08 12:02:41 +0200

    Création du formulaire, debut de bootstrap sur index

--- musiques/views.py
+++ musiques/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-#from task.models import Album, Musique, Artiste, Label
 from django.forms import ModelForm
 from django.contrib import messages
+from musiques.models import Artiste, Musique, Album
+from django import forms
 
 # Create your views here.
 def index(request):
-    return render(request, template_name='index.html')
+    search_form = Recherche()
+    return render(request, 'index.html', {'search': search_form})
+
+"""
+class RechercheArtiste(ModelForm):
+    class Meta:
+        model = Artiste
+        fields = ('nom_artiste',)
+
+class RechercheAlbum(ModelForm):
+    class Meta:
+        model = Album
+        fields = ('nom_album',)
+
+class RechercheMusique(ModelForm):
+    class Meta:
+        model = Musique
+        fields = ('titre_musique',)
+"""
+
+class Recherche(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(Recherche, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+    search = forms.CharField(max_length=250)
+    
