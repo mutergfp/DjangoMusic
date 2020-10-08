8baa1b9b27682aec3333bfcf5081ba33256d5e9a
Lucas <lucas.desnoue@free.fr>
2020-10-08 11:32:39 +0200

    Ajout des urls du sites + les vues correspondantes (à modifier plus tard) / Ajout des images par défaut de l'album et de l'artiste

--- musiques/urls.py
+++ musiques/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('result/?str={str}', views.resultRecherche, name='resultRecherche'),
+    path('albums/{id}', views.albumsDetail, name='albumsDetail'),
+    path('musiques/{id}', views.musiquesDetail, name='musiquesDetail'),
+    path('artistes/{id}', views.artistesDetail, name='artistesDetail'),
+    path('credit', views.credit, name='credit')
 ]
--- musiques/views.py
+++ musiques/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-#from task.models import Album, Musique, Artiste, Label
 from django.forms import ModelForm
 from django.contrib import messages
 
 # Create your views here.
 def index(request):
+    return render(request, template_name='index.html')
+
+def resultRecherche(request):
+    return render(request, template_name='index.html')
+
+def musiquesDetail(request, id):
+    return render(request, template_name='index.html')
+
+def artistesDetail(request, id):
+    return render(request, template_name='index.html')
+
+def albumsDetail(request, id):
+    return render(request, template_name='index.html')
+
+def credit(request):
     return render(request, template_name='index.html')
