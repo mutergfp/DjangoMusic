bc9c369f9be8076bbaeff4518ad04951fc457df1
Lucas <lucas.desnoue@free.fr>
2020-10-09 11:00:05 +0200

    Update des vues Musique, Album, Artiste et crédit

--- musiques/urls.py
+++ musiques/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('result/?str={str}', views.resultRecherche, name='resultRecherche'),
-    path('albums/{id}', views.albumsDetail, name='albumsDetail'),
-    path('musiques/{id}', views.musiquesDetail, name='musiquesDetail'),
-    path('artistes/{id}', views.artistesDetail, name='artistesDetail'),
+    path('resultatRecherche/', views.resultatRecherche, name='resultatRecherche'),
+    path('albums/<int:id>', views.albumsDetail, name='albumsDetail'),
+    path('musiques/<int:id>', views.musiquesDetail, name='musiquesDetail'),
+    path('artistes/<int:id>', views.artistesDetail, name='artistesDetail'),
     path('credit', views.credit, name='credit')
 ]
--- musiques/views.py
+++ musiques/views.py
@@ -2,22 +2,30 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import ModelForm
 from django.contrib import messages
+from musiques.models import Album, Artiste, Genre, Label, Musique
+import requests
 
 # Create your views here.
 def index(request):
     return render(request, template_name='index.html')
 
-def resultRecherche(request):
+def resultatRecherche(request):
     return render(request, template_name='index.html')
 
 def musiquesDetail(request, id):
-    return render(request, template_name='index.html')
+    if id != None :
+        musique = Musique.objects.get(id=id)
+    return render(request, template_name='index.html', context={'musique': musique})
 
 def artistesDetail(request, id):
-    return render(request, template_name='index.html')
+    if id != None :
+        artiste = Artiste.objects.get(id=id)
+    return render(request, template_name='index.html', context={'artiste': artiste})
 
 def albumsDetail(request, id):
-    return render(request, template_name='index.html')
+    if id != None :
+        album = Album.objects.get(id=id)
+    return render(request, template_name='index.html', context={'album': album})
 
 def credit(request):
     return render(request, template_name='index.html')
