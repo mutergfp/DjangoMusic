3942a5af35b40024a0d81c23b385cbe8623884d3
Erwan Gay <gay@info03-13.iut45.univ-orleans.fr>
2020-10-09 12:20:02 +0200

    template de resultat de recherche terminé, affichage de la recherche en cours de developpement

---
+++ musiques/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.forms import ModelForm
+from django.contrib import messages
+from musiques.models import Artiste, Musique, Album
+from django import forms
+
+# Create your views here.
+def index(request):
+
+    if(request.method == "POST"):
+        form = Recherche(request.POST)
+        if(form.is_valid()):
+            Artistes = Artiste.objects.get(nom_artiste__contains = request.POST)
+            return render(request, 'resultatRecherche.html', {'POST' : request.POST})
+    else:
+        search_form = Recherche()
+        return render(request, 'index.html', {'search': search_form})
+
+class Recherche(forms.Form):
+    def __init__(self, *args, **kwargs):
+        super(Recherche, self).__init__(*args, **kwargs)
+        for visible in self.visible_fields():
+            visible.field.widget.attrs['class'] = 'form-control'
+
+    search = forms.CharField(max_length=250)
+    
