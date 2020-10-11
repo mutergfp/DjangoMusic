aca6d919486bf03d9fa8a368583fdefe68869ba5
Wawan4137 <erwan.gaychanteloup@gmail.com>
2020-10-11 22:19:02 +0200

    Implémentation d'admin.py et des models de la branche developLucas. Modification de la fonctionnalité de recherche désormais utilisable via l'URL. Début de recherche dans la base de données après une recherche

--- musiques/models.py
+++ musiques/models.py
@@ -10,24 +10,36 @@ TYPE_ALBUM = (
 class Artiste(models.Model):
     nom_artiste = models.CharField(verbose_name=("Nom de l'artiste"), max_length=255, null=False)
     description_artiste = models.TextField(verbose_name=("Description de l'artiste"), null=True)
-    image_artiste = models.ImageField(verbose_name=("Image de l'artiste"), upload_to="assets/img_albums", null=True, default="assets/default/img_artiste_default.jpg")
+    image_artiste = models.ImageField(verbose_name=("Image de l'artiste"), upload_to="assets/img_albums", null=True, default="static/default/img_artiste_default-min.jpg")
+
+    def __str__(self):
+        return self.nom_artiste
 
 class Label(models.Model):
     nom_label = models.CharField(verbose_name=("Nom du Label"), max_length=100, null=False)
     description_label = models.TextField(verbose_name=("Description du Label"), null=True)
 
+    def __str__(self):
+        return self.nom_label
+
 class Genre(models.Model):
     nom_genre = models.CharField(verbose_name=("Nom du genre"), max_length=100)
     description_genre = models.CharField(verbose_name=("Description du genre"), max_length=50)
 
+    def __str__(self):
+        return self.nom_genre
+
 class Album(models.Model):
     nom_album = models.CharField(verbose_name=("Nom de la l'album"), max_length=100)
     type_album = models.CharField(verbose_name=("Type de l'album"), max_length=6, choices=TYPE_ALBUM)
-    image_album = models.ImageField(verbose_name=("Image de l'album"), upload_to="assets/img_artistes", null=False, default="assets/default/img_album_default.jpg")
+    image_album = models.ImageField(verbose_name=("Image de l'album"), upload_to="assets/img_artistes", null=False, default="static/assets/default/img_album_default-min.jpg")
     date_publication_album = models.DateField(verbose_name=("Date de parution de l'album"), null=True)
     id_artiste = models.ForeignKey(Artiste, verbose_name=("Artiste de l'album"), on_delete=models.CASCADE, null=False)
     id_label = models.ForeignKey(Label, verbose_name=("Label de l'album"), on_delete=models.CASCADE, null=False)
     id_genre = models.ForeignKey(Genre, verbose_name=("Genre de l'album"), on_delete=models.CASCADE, null=False)
+
+    def __str__(self):
+        return self.nom_album + ' (' + str(self.date_publication_album.year) + ')'
 
 class Musique(models.Model):
     titre_musique = models.CharField(verbose_name=("Titre de la musique"), max_length=255, null=False)
@@ -35,3 +47,6 @@ class Musique(models.Model):
     #Relation ManyToMany ATTENTION
     id_artiste = models.ManyToManyField(Artiste, verbose_name=("Artiste(s) de la musique"))
     id_album = models.ForeignKey(Album, verbose_name=("Album de la musiques"), on_delete=models.CASCADE, related_name="albums")
+
+    def __str__(self):
+        return self.titre_musique
--- musiques/views.py
+++ musiques/views.py
@@ -1,21 +1,11 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.forms import ModelForm
 from django.contrib import messages
 from musiques.models import Artiste, Musique, Album
 from django import forms
 
 # Create your views here.
-def index(request):
-
-    if(request.method == "POST"):
-        form = Recherche(request.POST)
-        if(form.is_valid()):
-            Artistes = Artiste.objects.get(nom_artiste__contains = request.POST)
-            return render(request, 'resultatRecherche.html', {'POST' : request.POST})
-    else:
-        search_form = Recherche()
-        return render(request, 'index.html', {'search': search_form})
 
 class Recherche(forms.Form):
     def __init__(self, *args, **kwargs):
@@ -24,4 +14,33 @@ class Recherche(forms.Form):
             visible.field.widget.attrs['class'] = 'form-control'
 
     search = forms.CharField(max_length=250)
+
+def index(request):
+    if(request.method == "POST"):
+        form = Recherche(request.POST)
+        if(form.is_valid()):
+            return redirect('search/'+request.POST['search'])
+    else:
+        search_form = Recherche()
+        return render(request, 'index.html', {'search': search_form})
+
+def resultSearch(request, text=None):
+    if(text != None):
+        try:
+            resultArtiste = Artiste.objects.get(nom_artiste = text)
+        except Artiste.DoesNotExist:
+            resultArtiste = "Aucun résultat trouvé"
+        
+        try:
+            resultAlbum = Album.objects.get(nom_album = text)
+        except Album.DoesNotExist:
+            resultAlbum = "Aucun résultat trouvé"
+        
+        try:
+            resultMusique = Musique.objects.get(titre_musique = text)
+        except Musique.DoesNotExist:
+            resultMusique = "Aucun résultat trouvé"
+
+        return render(request, 'resultatRecherche.html', {'text': text, 'artiste' : resultArtiste, 'album' : resultAlbum, 'musique': resultMusique})
+
     
