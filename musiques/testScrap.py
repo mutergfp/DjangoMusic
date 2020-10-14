89ec1a344818df9ce9b9a60249169bad34662c8d
Wawan4137 <erwan.gaychanteloup@gmail.com>
2020-10-14 17:57:18 +0200

    API Spotify et Scrapping allformusic pour l'ajout d'un artiste entièrement fonctionnel, relié avec la page de détails artiste

--- musiques/testScrap.py
+++ musiques/testScrap.py
@@ -1,5 +1,21 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+import requests
+from bs4 import BeautifulSoup
+
+def scrap_desc_artiste(artiste):
+
+    
+
+    html = requests.get("https://www.allformusic.fr/"+artiste.lower()).text
+
+    soup = BeautifulSoup(html, 'lxml')
+
+    ArtisteDescription = soup.find('div', class_ = "read")
+    if(ArtisteDescription == None):
+        return "Pas de description disponnible pour cet artiste"
+    else:
+        return ArtisteDescription.text
 
 cid ="b462b99712f64dcb94f3aab35a21827a"
 secret="cf75ea5330b44a8d8024d8ecc31c8b52"
@@ -12,8 +28,13 @@ client_credentials_manager)
 name="Booba"
 
 artistes = sp.search(q="artist:"+name, type="artist")
-
-print(artistes['artists']['href'])
+tab = []
 
 for artiste in artistes['artists']['items']:
-    print(artiste['name'])
+    tab.append(artiste['name'])
+
+desc = []
+
+for i in range(len(tab)):
+    desc.append(scrap_desc_artiste(tab[i]))
+
--- musiques/views.py
+++ musiques/views.py
@@ -7,6 +7,8 @@ from django import forms
 from musiques.functions.utils import totalDuree
 import requests
 from bs4 import BeautifulSoup
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # Create your views here.
 
@@ -34,6 +36,7 @@ def resultSearch(request, text=None):
         except Recherche.DoesNotExist:
             r = Recherche(contenu_recherche=text, compteur_recherche=1)
             r.save()
+            spotify_create_artiste(text)
         else:
             issetRecherche.compteur_recherche += 1
             issetRecherche.save()
@@ -87,10 +90,36 @@ def credit(request):
     return render(request, template_name='credit.html')
 
 def scrap_desc_artiste(artiste):
-    html = requests.get("https://www.allformusic.fr/.{artiste}"+format(artiste)).text
+ 
+    html = requests.get("https://www.allformusic.fr/"+artiste.lower()).text
 
     soup = BeautifulSoup(html, 'lxml')
 
     ArtisteDescription = soup.find('div', class_ = "read")
-    return ArtisteDescription.text
+    if(ArtisteDescription == None):
+        return "Pas de description disponnible pour cet artiste"
+    else:
+        return ArtisteDescription.text
 
+
+def spotify_create_artiste(name_artiste):
+    cid ="b462b99712f64dcb94f3aab35a21827a"
+    secret="cf75ea5330b44a8d8024d8ecc31c8b52"
+
+    client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+    sp = spotipy.Spotify(client_credentials_manager
+    =
+    client_credentials_manager)
+
+    artistes = sp.search(q="artist:"+name_artiste, type="artist")
+    tab = []
+
+    for artiste in artistes['artists']['items']:
+        tab.append(artiste['name'])
+
+    desc = []
+
+    for i in range(len(tab)):
+        desc.append(scrap_desc_artiste(tab[i]))
+        A = Artiste(nom_artiste=tab[i], description_artiste=desc[i])
+        A.save()
