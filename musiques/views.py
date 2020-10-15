fc8945e1de7d86f47375a5f9322ca1608cbd129e
Lucas <lucas.desnoue@free.fr>
2020-10-15 08:52:40 +0200

    Intergration plus poussé

--- musiques/views.py
+++ musiques/views.py
@@ -15,76 +15,104 @@ class RechercheForm(forms.Form):
         super(RechercheForm, self).__init__(*args, **kwargs)
         for visible in self.visible_fields():
             visible.field.widget.attrs['class'] = 'form-control'
+            visible.field.widget.attrs['placeholder'] = 'Artiste / Albums / Musique'
 
     search = forms.CharField(max_length=250)
 
-def index(request):
+def traitementRecherche(request):
     if(request.method == "POST"):
         form = RechercheForm(request.POST)
         if(form.is_valid()):
-            return redirect('search/'+request.POST['search'])
+            return redirect('/search/'+request.POST['search'])
     else:
-        search_form = RechercheForm()
+        return RechercheForm()
+
+
+def index(request):
+    search_form = traitementRecherche(request)
+    if not isinstance(search_form, RechercheForm):
+        return search_form
+    else:
         return render(request, 'index.html', {'search': search_form})
 
 def resultSearch(request, text=None):
-    if(text != None):
-        try: 
-            issetRecherche = Recherche.objects.get(contenu_recherche = text)
-        except Recherche.DoesNotExist:
-            r = Recherche(contenu_recherche=text, compteur_recherche=1)
-            r.save()
-        else:
-            issetRecherche.compteur_recherche += 1
-            issetRecherche.save()
+    search_form = traitementRecherche(request)
+    if not isinstance(search_form, RechercheForm):
+        return search_form
+    else:
+        if(text != None):
+            try: 
+                issetRecherche = Recherche.objects.get(contenu_recherche = text)
+            except Recherche.DoesNotExist:
+                r = Recherche(contenu_recherche=text, compteur_recherche=1)
+                r.save()
+            else:
+                issetRecherche.compteur_recherche += 1
+                issetRecherche.save()
 
-        try:
-            resultArtiste = Artiste.objects.get(nom_artiste = text)
-        except Artiste.DoesNotExist:
-            resultArtiste = "Aucun résultat trouvé"
-        
-        try:
-            resultAlbum = Album.objects.get(nom_album = text)
-        except Album.DoesNotExist:
-            resultAlbum = "Aucun résultat trouvé"
-        
-        try:
-            resultMusique = Musique.objects.get(titre_musique = text)
-        except Musique.DoesNotExist:
-            resultMusique = "Aucun résultat trouvé"
+            try:
+                resultArtiste = Artiste.objects.get(nom_artiste = text)
+            except Artiste.DoesNotExist:
+                resultArtiste = "Aucun résultat trouvé"
+            
+            try:
+                resultAlbum = Album.objects.get(nom_album = text)
+            except Album.DoesNotExist:
+                resultAlbum = "Aucun résultat trouvé"
+            
+            try:
+                resultMusique = Musique.objects.get(titre_musique = text)
+            except Musique.DoesNotExist:
+                resultMusique = "Aucun résultat trouvé"
 
-        return render(request, 'resultatRecherche.html', {'text': text, 'artiste' : resultArtiste, 'album' : resultAlbum, 'musique': resultMusique})
-    return render(request, template_name='index.html')
+            return render(request, 'resultatRecherche.html', {'search': search_form, 'text': text, 'artiste' : resultArtiste, 'album' : resultAlbum, 'musique': resultMusique})
+        return render(request, template_name='index.html')
 
 
 def musiquesDetail(request, id):
-    if id != None :
-        musique = Musique.objects.get(id=id)
-    return render(request, template_name='index.html', context={'musique': musique})
+    search_form = traitementRecherche(request)
+    if not isinstance(search_form, RechercheForm):
+        return search_form
+    else:
+        if id != None :
+            musique = Musique.objects.get(id=id)
+        return render(request, template_name='index.html', context={'search': search_form, 'musique': musique})
 
 def artistesDetail(request, id):
-    if id != None :
-        artiste = Artiste.objects.get(id=id)
-        albums = Album.objects.all().filter(id_artiste=artiste.id).order_by('-date_publication_album')
-        musiques = Musique.objects.none()
-        for album in albums:
-            musiques = musiques | Musique.objects.all().filter(id_album=album.id)
-            #musiques = {**musiques, **musiquesTmp}
-        return render(request, template_name='artistesDetail.html', context={'artiste': artiste, 'albums': albums, 'musiques': musiques})
+    search_form = traitementRecherche(request)
+    if not isinstance(search_form, RechercheForm):
+        return search_form
+    else:
+        if id != None :
+            artiste = Artiste.objects.get(id=id)
+            albums = Album.objects.all().filter(id_artiste=artiste.id).order_by('-date_publication_album')
+            musiques = Musique.objects.none()
+            for album in albums:
+                musiques = musiques | Musique.objects.all().filter(id_album=album.id)
+                #musiques = {**musiques, **musiquesTmp}
+            return render(request, template_name='artistesDetail.html', context={'search': search_form, 'artiste': artiste, 'albums': albums, 'musiques': musiques})
 
 def albumsDetail(request, id):
-    if id != None :
-        album = Album.objects.get(id=id)
-        musiques = Musique.objects.all().filter(id_album=album.id)
-        extraData = {}
-        times = []
-        for musique in musiques:
-            times.append(musique.duree_musique)
-        extraData['totalTime'] = totalDuree(times)
-        return render(request, template_name='albumsDetail.html', context={'album': album, 'musiques': musiques, 'extraData': extraData})
+    search_form = traitementRecherche(request)
+    if not isinstance(search_form, RechercheForm):
+        return search_form
+    else:
+        if id != None :
+            album = Album.objects.get(id=id)
+            musiques = Musique.objects.all().filter(id_album=album.id)
+            extraData = {}
+            times = []
+            for musique in musiques:
+                times.append(musique.duree_musique)
+            extraData['totalTime'] = totalDuree(times)
+            return render(request, template_name='albumsDetail.html', context={'search': search_form, 'album': album, 'musiques': musiques, 'extraData': extraData})
 
 def credit(request):
-    return render(request, template_name='credit.html')
+    search_form = traitementRecherche(request)
+    if not isinstance(search_form, RechercheForm):
+        return search_form
+    else:
+        return render(request, template_name='credit.html', context={'search': search_form})
 
 def scrap_desc_artiste(artiste):
     html = requests.get("https://www.allformusic.fr/.{artiste}"+format(artiste)).text
